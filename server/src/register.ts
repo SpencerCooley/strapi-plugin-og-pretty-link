5fef8eda41f14d2f0567f30ac1dc5f5ba95d3a32
Spencer Cooley <contact@spencercooley.com>
2025-09-13 18:29:47 -0500

    successfully added backend needs and have the component loading in the right place.

--- server/src/register.ts
+++ server/src/register.ts
@@ -2,6 +2,11 @@ import type { Core } from '@strapi/strapi';
 
 const register = ({ strapi }: { strapi: Core.Strapi }) => {
   console.log('Successfully registered the og-pretty-link plugin.');
+  strapi.customFields.register({
+    name: 'og-link',
+    plugin: 'og-pretty-link',
+    type: 'json',
+  });
 };
 
 export default register;
