4f328b76dc6d70e41459117edeee320f7824480d
Spencer Cooley <contact@spencercooley.com>
2025-09-14 02:26:19 -0500

    chore: bump dependencies and patch

--- server/src/register.ts
+++ server/src/register.ts
@@ -1,7 +1,6 @@
 import type { Core } from '@strapi/strapi';
 
 const register = ({ strapi }: { strapi: Core.Strapi }) => {
-  console.log('Successfully registered the og-pretty-link plugin.');
   strapi.customFields.register({
     name: 'og-link',
     plugin: 'og-pretty-link',
